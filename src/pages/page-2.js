2638adaa11e34a906c08db2efe4cf8ba4c260085
Rasmi <81720844+cool-coder-here@users.noreply.github.com>
2021-12-13 21:33:19 +0530

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,7 +7,7 @@ import Seo from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
-    <h1>Welcome to the Page of quotes.</h1>
+    <h1>Welcome to the Page of Quotes.</h1>
     <p>Quotes Page</p>
   <p>
   <h3>Dreams</h3>
